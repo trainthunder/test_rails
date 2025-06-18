0d4929d7bf48e5181bc80c015a309fb4ac70e6cf
Kitikun <kitikun.po@gmail.com>
2025-06-18 21:13:45 +0700

    feat: full system

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,4 +1,50 @@
 class QuestsController < ApplicationController
+  before_action :set_quest, only: [ :update, :destroy ]
+
   def index
+    @quests = Quest.all
+    @quest = Quest.new
+  end
+
+  def new
+    @quest = Quest.new
+  end
+
+  def create
+    @quest = Quest.new(quest_params)
+    if @quest.save
+      respond_to do |format|
+        # format.turbo_stream
+        format.html { redirect_to quests_path }
+      end
+    else
+      render :new
+    end
+  end
+
+  def update
+    @quest.update(status: !@quest.status)
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to quests_path }
+    end
+  end
+
+  def destroy
+    @quest.destroy
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to quests_path }
+    end
+  end
+
+  private
+
+  def set_quest
+    @quest = Quest.find(params[:id])
+  end
+
+  def quest_params
+    params.require(:quest).permit(:title, :status)
   end
 end
--- spec/requests/quests_spec.rb
+++ spec/requests/quests_spec.rb
@@ -1,11 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe "Quests", type: :request do
-  describe "GET /index" do
-    # pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /quests" do
     it 'render index page' do
       get quests_path
       expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST /quests" do
+    it "creates a new quest" do
+      expect {
+        post quests_path, params: { quest: { title: "New Quest", status: false } }
+      }.to change(Quest, :count).by(1)
+
+      expect(response).to redirect_to(quests_path)
+    end
+  end
+
+  describe "PATCH /quests/:id" do
+    it "toggles the quest status" do
+      quest = Quest.create!(title: "Toggle Quest", status: false)
+
+      patch quest_path(quest)
+      quest.reload
+
+      expect(quest.status).to eq(true)
+    end
+  end
+
+  describe "DELETE /quests/:id" do
+    it "deletes the quest" do
+      quest = Quest.create!(title: "Delete Me", status: false)
+
+      expect {
+        delete quest_path(quest)
+      }.to change(Quest, :count).by(-1)
+
+      expect(response).to redirect_to(quests_path)
     end
   end
 end
